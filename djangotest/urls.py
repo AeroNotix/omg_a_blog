46afade7a970c971e72705495c780fa9d53f39c6
Aaron France <aaron.l.france@gmail.com>
2012-03-10 14:10:10 +0100

    heroku

--- djangotest/urls.py
+++ djangotest/urls.py
@@ -4,4 +4,5 @@ from djangotest.views import *
 
 urlpatterns = patterns('',
     url(r'^$', index),
+    url(r'^time/(\d{1,2})/$', get_time),
 )
--- djangotest/views.py
+++ djangotest/views.py
@@ -2,8 +2,36 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from django.template import RequestContext
 from django.views.decorators.csrf import csrf_protect
-from django.template.loader import get_template
+from django.template import Template, Context
+
+import datetime
 
 def index(request):
 
     return HttpResponse("Hello world")
+
+def get_time(request, time_diff):
+
+    offset = int(time_diff)
+    time_diff = datetime.datetime.now() + datetime.timedelta(hours=int(time_diff))
+    
+    template = Template('''
+                        in {{ offset }} {{ hour_word }} the time will be
+                        {{time_diff }}
+                        ''')
+
+    context = Context(
+        {
+        'offset': offset,
+        'time_diff': time_diff
+        }
+    )
+    
+    if offset > 1:
+        context['hour_word'] = 'hours'
+    else:
+        context['hour_word'] = 'hour'
+
+    return HttpResponse(template.render(context))
+    
+                      
