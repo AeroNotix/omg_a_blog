926347ff8a97f529937b4a9465e52ce1ba0327e2
Aaron France <aaron.l.france@gmail.com>
2012-03-25 00:01:46 +0100

    Collect static command

--- djangotest/blog/models.py
+++ djangotest/blog/models.py
@@ -5,3 +5,6 @@ class Blog(models.Model):
     date = models.DateField(auto_now_add=True)
     blog_title = models.CharField(max_length=64)
     blog_post = models.TextField()
+
+    def __unicode__(self):
+        return self.blog_title
