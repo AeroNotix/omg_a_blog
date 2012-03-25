8623dd9f4d448b9fdd08f9b8a2dcf1389ae69a6a
Aaron France <aaron.l.france@gmail.com>
2012-03-25 21:35:23 +0200

    Adding a field

--- djangotest/blog/models.py
+++ djangotest/blog/models.py
@@ -4,6 +4,7 @@ class Blog(models.Model):
 
     date = models.DateField(auto_now_add=True)
 
+    blog_url = models.CharField(max_length=64, unique=True)
     blog_title = models.CharField(max_length=64)
     blog_post = models.TextField()
 
