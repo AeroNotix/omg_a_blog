e5ee86eb9736f708c66cb3973957de21a7eb2e12
Aaron France <aaron.l.france@gmail.com>
2012-03-25 20:57:15 +0200

    Styling issues with code snippet box

--- djangotest/blog/models.py
+++ djangotest/blog/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class Blog(models.Model):
 
     date = models.DateField(auto_now_add=True)
+
+    blog_url = models.CharField(max_length=64)
     blog_title = models.CharField(max_length=64)
     blog_post = models.TextField()
 
